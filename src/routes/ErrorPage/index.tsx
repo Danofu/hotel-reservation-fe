c13955e382acdbe1144e0af478477266c858676d
Daniil Borysov <danofu13@gmail.com>
2022-10-13 13:29:48 +0200

    Style error page
    * add dependencies:
      * @emotion/react
      * @emotion/styled
      * @mui/material
    * add Roboto font

--- src/routes/ErrorPage/index.tsx
+++ src/routes/ErrorPage/index.tsx
@@ -1,10 +1,50 @@
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
 import React, { FC } from 'react';
-import { isRouteErrorResponse, useRouteError } from 'react-router-dom';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import { isRouteErrorResponse, Link as RouterLink, useRouteError } from 'react-router-dom';
 
 const ErrorPage: FC = () => {
   const error = useRouteError();
 
-  return <h1>{isRouteErrorResponse(error) ? `${error.status}: ${error.statusText}` : 'Oops...'}</h1>;
+  return (
+    <Container
+      maxWidth={false}
+      sx={{
+        alignItems: 'center',
+        bgcolor: '#EFEFEF',
+        display: 'flex',
+        height: '100vh',
+        justifyContent: 'center',
+        py: 3,
+        textAlign: 'center',
+        width: '100%',
+      }}
+    >
+      <Stack spacing={3}>
+        <Typography component="h1" variant="h4">
+          Oops... Something went wrong !
+        </Typography>
+        <Typography component="h3" variant="h5">
+          {isRouteErrorResponse(error)
+            ? `${error.status} ${error.statusText}`
+            : 'Sorry, an unexpected error has occurred.'}
+        </Typography>
+        <Typography component="p" variant="body1">
+          Try our&nbsp;
+          <Link component={RouterLink} to="home" underline="hover">
+            home
+          </Link>
+          &nbsp;or&nbsp;
+          <Link component={RouterLink} to="#" underline="hover">
+            login
+          </Link>
+          &nbsp;page
+        </Typography>
+      </Stack>
+    </Container>
+  );
 };
 
 export default ErrorPage;
