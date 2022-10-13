37c6683399f675e7707b4f8a40248c811f6bb96a
Daniil Borysov <danofu13@gmail.com>
2022-10-13 18:31:29 +0200

    Add formik and yup validation

--- src/components/AuthorizationForm/index.tsx
+++ src/components/AuthorizationForm/index.tsx
@@ -1,49 +1,103 @@
-import Button from '@mui/material/Button';
+import * as Yup from 'yup';
 import Checkbox from '@mui/material/Checkbox';
 import FormControlLabel from '@mui/material/FormControlLabel';
-import React, { FC, FormEvent } from 'react';
+import LoadingButton from '@mui/lab/LoadingButton';
+import React, { FC } from 'react';
 import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
+import { Form, Formik, FormikHelpers, FormikProps } from 'formik';
 
-const AuthorizationForm: FC = () => {
-  const submitHandler = (event: FormEvent) => {
-    event.preventDefault();
-  };
+export type FormValues = {
+  email: string;
+  password: string;
+  remember: boolean;
+};
+
+const InnerAuthorizationForm: FC<FormikProps<FormValues>> = ({
+  errors,
+  handleBlur,
+  handleChange,
+  isSubmitting,
+  touched,
+  values,
+}) => {
+  const isEmailValid = touched.email && !!errors.email;
+  const isPasswordValid = touched.password && !!errors.password;
 
   return (
-    <Stack component="form" noValidate onSubmit={submitHandler} spacing={3}>
+    <Stack component={Form} noValidate spacing={3}>
       <TextField
         autoComplete="email"
-        autoFocus
-        color="warning"
+        error={isEmailValid}
         fullWidth
+        helperText={isEmailValid && errors.email}
         label="Email"
         margin="none"
         name="email"
+        onBlur={handleBlur}
+        onChange={handleChange}
         required
         type="email"
+        value={values.email}
       />
       <TextField
         autoComplete="current-password"
-        color="warning"
+        error={isPasswordValid}
         fullWidth
+        helperText={isPasswordValid && errors.password}
         label="Password"
         margin="none"
         name="password"
+        onBlur={handleBlur}
+        onChange={handleChange}
         required
         type="password"
+        value={values.password}
       />
       <FormControlLabel
-        control={<Checkbox color="warning" sx={{ mr: -1 }} value="remember" />}
-        disabled
+        control={<Checkbox sx={{ mr: -1 }} checked={values.remember} />}
         label="Remember me"
         labelPlacement="start"
+        name="remember"
+        onBlur={handleBlur}
+        onChange={handleChange}
       />
-      <Button color="warning" fullWidth type="submit" variant="contained">
-        Sign In
-      </Button>
+      <LoadingButton disabled={isSubmitting} fullWidth loading={isSubmitting} type="submit" variant="contained">
+        Sign in
+      </LoadingButton>
     </Stack>
   );
 };
+
+type Props = {
+  initialValues?: FormValues;
+  onSubmit: (values: FormValues, formikHelpers: FormikHelpers<FormValues>) => void | Promise<unknown>;
+  validationSchema?: unknown | (() => unknown);
+};
+
+const defaultInitialValues: FormValues = {
+  email: '',
+  password: '',
+  remember: false,
+};
+
+const defaultValidationSchema = Yup.object({
+  email: Yup.string().required('Provide an email').email('Provide a valid email'),
+  password: Yup.string().required('Provide a password').min(6, 'Provide a password with at least 6 symbols'),
+  remember: Yup.boolean(),
+});
+
+const AuthorizationForm: FC<Props> = ({
+  initialValues = defaultInitialValues,
+  onSubmit,
+  validationSchema = defaultValidationSchema,
+}) => (
+  <Formik
+    component={InnerAuthorizationForm}
+    initialValues={initialValues}
+    onSubmit={onSubmit}
+    validationSchema={validationSchema}
+  />
+);
 
 export default AuthorizationForm;
--- src/routes/LoginPage/index.tsx
+++ src/routes/LoginPage/index.tsx
@@ -3,32 +3,42 @@ import Paper from '@mui/material/Paper';
 import React, { FC } from 'react';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
+import { FormikHelpers } from 'formik';
 
-import AuthorizationForm from 'src/components/AuthorizationForm';
+import AuthorizationForm, { FormValues } from 'src/components/AuthorizationForm';
 
-const LoginPage: FC = () => (
-  <Container
-    maxWidth={false}
-    sx={{
-      alignItems: 'center',
-      bgcolor: '#EFEFEF',
-      display: 'flex',
-      height: '100vh',
-      justifyContent: 'center',
-      py: 3,
-      textAlign: 'center',
-      width: 1,
-    }}
-  >
-    <Paper elevation={3} sx={{ px: 5, py: 4, width: 500 }}>
-      <Stack spacing={3}>
-        <Typography align="center" component="h1" sx={{ textTransform: 'capitalize' }} variant="h5">
-          Authorization 😼
-        </Typography>
-        <AuthorizationForm />
-      </Stack>
-    </Paper>
-  </Container>
-);
+const LoginPage: FC = () => {
+  const handleSubmit = async (values: FormValues, formikHelpers: FormikHelpers<FormValues>) => {
+    // TODO: make authorize request
+    console.log(values);
+    await new Promise((r) => setTimeout(r, 10 * 1000));
+    formikHelpers.resetForm();
+  };
+
+  return (
+    <Container
+      maxWidth={false}
+      sx={{
+        alignItems: 'center',
+        bgcolor: '#EFEFEF',
+        display: 'flex',
+        height: '100vh',
+        justifyContent: 'center',
+        py: 3,
+        textAlign: 'center',
+        width: 1,
+      }}
+    >
+      <Paper elevation={3} sx={{ px: 5, py: 4, width: 500 }}>
+        <Stack spacing={6}>
+          <Typography align="center" component="h1" sx={{ textTransform: 'capitalize' }} variant="h5">
+            Authorization 😼
+          </Typography>
+          <AuthorizationForm onSubmit={handleSubmit} />
+        </Stack>
+      </Paper>
+    </Container>
+  );
+};
 
 export default LoginPage;
