e06d0c86250a19d9e3a5de7dca1b5691751286fa
Daniil Borysov <danofu13@gmail.com>
2022-10-17 23:15:45 +0200

    Add navigation

--- src/providers/ThemeProvider/theme.ts
+++ src/providers/ThemeProvider/theme.ts
@@ -1,21 +1,21 @@
 import { createTheme, ThemeOptions } from '@mui/material';
-import { grey, teal } from '@mui/material/colors';
+import { green, lightGreen, teal } from '@mui/material/colors';
 
 import { CreateThemeArgs } from 'src/providers/ThemeProvider/types';
 
 const lightThemeOptions: ThemeOptions = {
   palette: {
     mode: 'light',
-    primary: { main: teal[700] },
-    secondary: { main: grey[700] },
+    primary: { main: green[700] },
+    secondary: { main: teal[50] },
   },
 };
 
 const darkThemeOptions: ThemeOptions = {
   palette: {
     mode: 'dark',
-    primary: { main: teal[300] },
-    secondary: { main: grey[300] },
+    primary: { main: green[100] },
+    secondary: { main: lightGreen[800] },
   },
 };
 
