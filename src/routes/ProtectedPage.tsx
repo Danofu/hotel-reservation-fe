7095026455d1643a07b5fcf4c43000d0a1be4ebb
Daniil Borysov <danofu13@gmail.com>
2022-10-17 20:34:05 +0200

    Add protected page
    * move index page to unprotected route
    * add protected page to protected route

---
+++ src/routes/ProtectedPage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Typography from '@mui/material/Typography';
+import { Helmet } from 'react-helmet';
+
+const ProtectedPage = () => (
+  <>
+    <Helmet>
+      <title>Protected Page</title>
+    </Helmet>
+    <Typography align="center" component="h1" m={3} variant="h5">
+      Protected Page 😼
+    </Typography>
+  </>
+);
+
+export default ProtectedPage;
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -9,17 +9,28 @@ const AppLayout = lazy(() => import('src/routes/AppLayout'));
 const ErrorPage = lazy(() => import('src/routes/ErrorPage'));
 const IndexPage = lazy(() => import('src/routes/IndexPage'));
 const LoginPage = lazy(() => import('src/routes/LoginPage'));
+const ProtectedPage = lazy(() => import('src/routes/ProtectedPage'));
 
 export default createBrowserRouter([
   {
     children: [
       {
-        children: [{ element: <Suspense component={<IndexPage />} fallback={<LoadingBackdrop />} />, index: true }],
+        children: [
+          {
+            element: <Suspense component={<ProtectedPage />} fallback={<LoadingBackdrop />} />,
+            path: 'protected',
+          },
+        ],
         element: <Suspense component={<AppLayout />} fallback={<LoadingBackdrop />} />,
       },
     ],
     element: <ProtectedRoutes />,
     errorElement: <Suspense component={<ErrorPage />} fallback={<LoadingBackdrop />} />,
+    path: '/',
+  },
+  {
+    children: [{ element: <Suspense component={<IndexPage />} fallback={<LoadingBackdrop />} />, index: true }],
+    element: <Suspense component={<AppLayout />} fallback={<LoadingBackdrop />} />,
     path: '/',
   },
   {
