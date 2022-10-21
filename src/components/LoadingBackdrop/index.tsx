69cfb9912fd9d1d0dc3c820a8370347e5e205816
Daniil Borysov <danofu13@gmail.com>
2022-10-21 18:21:54 +0200

    Add loading backdrop delay

--- src/components/LoadingBackdrop/index.tsx
+++ src/components/LoadingBackdrop/index.tsx
@@ -1,17 +1,27 @@
 import Backdrop from '@mui/material/Backdrop';
 import CircularProgress from '@mui/material/CircularProgress';
 import Portal from '@mui/material/Portal';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { grey } from '@mui/material/colors';
 
 import { loadingContainer } from 'src/components/LoadingBackdrop/constants';
 
-const LoadingBackdrop = () => (
-  <Portal container={loadingContainer}>
-    <Backdrop open sx={{ color: grey[50], zIndex: (theme) => theme.zIndex.drawer + 1 }}>
-      <CircularProgress color="inherit" />
-    </Backdrop>
-  </Portal>
-);
+const LoadingBackdrop = () => {
+  const [open, setOpen] = useState<boolean>(false);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(setOpen, 1000, true);
+
+    return () => clearTimeout(timeoutId);
+  }, []);
+
+  return (
+    <Portal container={loadingContainer}>
+      <Backdrop open={open} sx={{ color: grey[50], zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+        <CircularProgress color="inherit" />
+      </Backdrop>
+    </Portal>
+  );
+};
 
 export default LoadingBackdrop;
