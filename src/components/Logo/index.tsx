d468619638154eed1c8d34ac23f9670510bdc72f
Daniil Borysov <danofu13@gmail.com>
2022-11-13 17:51:42 +0100

    Change logo icon

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,15 +1,16 @@
-import KingBedIcon from '@mui/icons-material/KingBed';
+import NightShelterIcon from '@mui/icons-material/NightShelter';
 import React, { FC } from 'react';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 
 import Link from 'components/utils/Link';
+import pop from 'utils/pop';
 import { LOGO_TEXT_SHORT } from 'app-constants';
 import { Props } from 'components/Logo/types';
 
-const Logo: FC<Props> = ({ IconProps, TextProps, text = LOGO_TEXT_SHORT, ...props }) => (
+const Logo: FC<Props> = ({ IconProps = {}, TextProps, text = LOGO_TEXT_SHORT, ...props }) => (
   <Stack alignItems="center" component={Link} direction="row" gap={1} {...props}>
-    <KingBedIcon {...IconProps} />
+    <NightShelterIcon sx={{ mt: -0.5, ...pop(IconProps, 'sx') }} {...IconProps} />
     <Typography {...TextProps}>{text}</Typography>
   </Stack>
 );
