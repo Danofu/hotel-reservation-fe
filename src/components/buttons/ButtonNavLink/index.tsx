203b38222b5962e2430958557c2efc0b5c12b85d
Daniil Borysov <danofu13@gmail.com>
2022-11-17 05:09:11 +0100

    Document ButtonNavLink

--- src/components/buttons/ButtonNavLink/index.tsx
+++ src/components/buttons/ButtonNavLink/index.tsx
@@ -1,10 +1,37 @@
+/**
+ * The `ButtonNavLink` component.
+ * @module ButtonNavLink
+ */
 import Button from '@mui/material/Button';
 import React, { FC } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
-import { Props } from 'components/buttons/ButtonNavLink/types';
+import { Props as ButtonNavLinkProps } from 'components/buttons/ButtonNavLink/types';
 
-const ButtonNavLink: FC<Props> = ({ to, ...props }) => {
+/**
+ * Renders a navigation button link based on **Material UI** {@link https://mui.com/material-ui/react-button | Button} and **React Router Dom** {@link https://reactrouter.com/en/main/components/link | Link}.
+ *
+ * The link will be **disabled** if current URL and link's navigation pathname (`to` property) matches.
+ *
+ * The component declaration:
+ * ```ts
+ * const ButtonNavLink: FC<ButtonNavLinkProps> = ({ to, ...props }) => {
+ *   // ...
+ * }
+ * ```
+ *
+ * @example
+ * ```tsx
+ * <Fragment>
+ *   <ButtonNavLink to="/home">Home</ButtonNavLink>
+ *   <ButtonNavLink to="/about">About</ButtonNavLink>
+ *   <ButtonNavLink to="/contacts">Contacts</ButtonNavLink>
+ * <Fragment/>
+ * ```
+ *
+ * @group Component
+ */
+const ButtonNavLink: FC<ButtonNavLinkProps> = ({ to, ...props }) => {
   const { pathname } = useLocation();
 
   return <Button component={Link} disabled={pathname === to} to={to} {...props} />;
@@ -12,4 +39,4 @@ const ButtonNavLink: FC<Props> = ({ to, ...props }) => {
 
 export default ButtonNavLink;
 
-export type ButtonNavLinkProps = Props;
+export type { ButtonNavLinkProps };
--- src/components/buttons/ButtonNavLink/types.ts
+++ src/components/buttons/ButtonNavLink/types.ts
@@ -1,4 +1,5 @@
 import { ButtonProps } from '@mui/material/Button';
 import { LinkProps } from 'react-router-dom';
 
+/** The {@link default | ButtonNavLink} properties type. */
 export type Props = ButtonProps<'a', LinkProps>;
