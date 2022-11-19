5aa1f7bffcbace7474a5438d42740a916ba216f6
Daniil Borysov <danofu13@gmail.com>
2022-11-19 16:55:15 +0100

    Use local fonts
    * add fonts dependency
    * remove Google fonts external import
    * import local fonts

--- src/index.tsx
+++ src/index.tsx
@@ -11,6 +11,10 @@ import Providers from 'providers/Providers';
 import ToastContainer from 'components/ToastContainer';
 import router from 'routes/router';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import 'react-toastify/dist/ReactToastify.min.css';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
