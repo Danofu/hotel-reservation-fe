8b1946d8b4274867b96218066fc0bf9893937e9c
Daniil Borysov <danofu13@gmail.com>
2022-11-30 15:13:17 +0100

    Create register API utility
    * create utility function to register a user
    * document the utility

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -5,6 +5,23 @@ const axiosInstance = axios.create({ baseURL: process.env.REACT_APP_API_URL, tim
 type AuthenticateUserResponse = { message: string; token: string };
 
 /**
+ * The arguments type of the utility function {@link registerUser}.
+ *
+ * @category API
+ */
+export type RegisterUserArgs = {
+  /** the email of the user */
+  email: string;
+  /** the family name of the user */
+  familyName: string;
+  /** the given name of the user */
+  givenName: string;
+  /** the password of the user */
+  password: string;
+};
+type RegisterUserResponse = { message: string };
+
+/**
  * Authenticates a user for the given `email` and `password`. Throws an error if request is invalid.
  *
  * @param email - the email of a user
@@ -17,3 +34,20 @@ type AuthenticateUserResponse = { message: string; token: string };
  */
 export const authenticateUser = async (email: string, password: string) =>
   axiosInstance.post<AuthenticateUserResponse>('/api/auth/login', { email, password });
+
+/**
+ * Registers the user with the given data. Throws an error if request is invalid.
+ *
+ * @param registerData - the data to register the user
+ *
+ * @throws {@link https://github.com/axios/axios/blob/v1.x/index.d.ts#L357 | AxiosError} if request is invalid.
+ *
+ * @category API
+ */
+export const registerUser = async ({ email, familyName, givenName, password }: RegisterUserArgs) =>
+  axiosInstance.post<RegisterUserResponse>('/api/auth/login', {
+    email,
+    firstName: givenName,
+    lastName: familyName,
+    password,
+  });
