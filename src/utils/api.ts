8c05c76e3f7c9c477c7ea16cdf83b1ad9af04339
Daniil Borysov <danofu13@gmail.com>
2022-11-30 12:42:52 +0100

    Create API utility for user authentication
    * create api utility that authenticates user
    * document the utility

---
+++ src/utils/api.ts
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({ baseURL: process.env.REACT_APP_API_URL, timeout: 30 * 1000 });
+
+/**
+ * Authenticates a user for the given `email` and `password`. Throws an error if request is invalid.
+ *
+ * @param email - the email of a user
+ * @param password - the password of a user
+ * @returns A response from the requested endpoint `/api/auth/login` from a backend.
+ *
+ * @throws {@link https://github.com/axios/axios/blob/v1.x/index.d.ts#L357 | AxiosError} if request is invalid.
+ *
+ * @category API
+ */
+export const authenticateUser = async (email: string, password: string) =>
+  axiosInstance.post('/api/auth/login', { email, password });
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,6 +2,7 @@
  * The application utility functions.
  * @module Utils
  */
+export * from 'utils/api';
 export * from 'utils/pop';
 export * from 'utils/storage';
 export { default as ntu } from 'utils/ntu';
