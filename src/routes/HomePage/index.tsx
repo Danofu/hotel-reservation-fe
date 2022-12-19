34676ee043f2ed5e3cebe0705abfb3a8daac585a
Daniil Borysov <danofu13@gmail.com>
2022-12-19 18:58:27 +0100

    Use room component
    * fetch rooms data
    * show rooms on the home page using room component

--- src/routes/HomePage/index.tsx
+++ src/routes/HomePage/index.tsx
@@ -1,13 +1,40 @@
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
 import React, { Fragment } from 'react';
 import Typography from '@mui/material/Typography';
 import { Helmet } from 'react-helmet';
 import { useTranslation } from 'react-i18next';
 
+import Room from 'components/Room';
 import { LOGO_TEXT_LONG } from 'app-globals';
+import { Room as RoomType } from 'routes/HomePage/types';
 import { TPATH } from 'routes/HomePage/constants';
+import { useAxios } from 'utils';
 
 const HomePage = () => {
   const { t } = useTranslation();
+  const { data: rooms } = useAxios<RoomType[]>('/api/kategorja/getAllPokoj');
+
+  let pageContent = <Typography>{t(`${TPATH}.noContent`)}</Typography>;
+
+  if (rooms?.length) {
+    pageContent = (
+      <Container maxWidth="xl" sx={{ mb: 2 }}>
+        <Grid container spacing={2}>
+          {rooms.map((room) => (
+            <Grid item key={room.id} xs={3}>
+              <Room
+                description={room.dodatkowa_informacja}
+                header={room.kategorja}
+                peopleAmount={room.ilosc_miejsc}
+                price={room.cena}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    );
+  }
 
   return (
     <Fragment>
@@ -17,6 +44,7 @@ const HomePage = () => {
       <Typography component="h1" m={3} variant="h5">
         {t(`${TPATH}.heading`)}
       </Typography>
+      {pageContent}
     </Fragment>
   );
 };
---
+++ src/routes/HomePage/types.ts
@@ -0,0 +1,8 @@
+export type Room = {
+  cena: number;
+  dodatkowa_informacja: string;
+  id: number;
+  ilosc_miejsc: number;
+  ilosc_mieszkan: number;
+  kategorja: string;
+};
